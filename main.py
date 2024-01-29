585444f1c8358eecbe589bc5de0b4e3efa0345e0
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-01-29 14:53:12 +0100

    adding buttons to app

--- main.py
+++ main.py
@@ -16,5 +16,14 @@ url_entry = Entry(root)
 canvas.create_window(200, 80, window=url_label)
 canvas.create_window(200, 100, window=url_entry)
 
+# path to download videos
+path_label = Label(root, text="select path where to download")
+bath_button = Button(root, text="Select")
+canvas.create_window(200, 150, window=path_label)
+canvas.create_window(200, 170, window=bath_button)
+
+# download button
+download_button = Button(root, text='Download')
+canvas.create_window(200, 200, window=download_button)
 
 root.mainloop()
