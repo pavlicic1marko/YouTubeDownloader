7a5005b36e810905b7401957930e53b5418dc1d6
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-01-30 17:44:36 +0100

    downloading playlist and adding to filename index of video

--- download_playlist.py
+++ download_playlist.py
@@ -1,4 +1,23 @@
 from tkinter import *
+from pytube import Playlist
+
+
+def download():
+    playlist_path = url_entry.get()
+    print('Downloading...')
+    playlist = Playlist(playlist_path)
+    print('Number of videos in playlist: %s' % len(playlist.video_urls))
+    for index, video in enumerate(playlist.videos, start=1):
+        try:
+            # Rename the downloaded file with a number prefix
+            original_filename = video.streams.get_highest_resolution().default_filename
+            new_filename = f"{index:02d}_{original_filename}"
+            video.streams.get_highest_resolution().download(filename=new_filename)
+
+            print(f"Downloaded video {index}: {video.title}")
+        except Exception as e:
+            print(f"Error downloading video {index}: {str(e)}")
+
 
 root = Tk()
 root.title('Video Downloader')
@@ -22,7 +41,7 @@ canvas.create_window(200, 150, window=path_label)
 canvas.create_window(200, 170, window=bath_button)
 
 # download button
-download_button = Button(root, text='Download', command='download')
+download_button = Button(root, text='Download', command=download)
 canvas.create_window(200, 200, window=download_button)
 
 root.mainloop()
